1735af963254214861dc7f0318a9b1d92f8bbb78
UnnatMalik <parvinder221978@gmail.com>
2025-01-09 20:08:51 +0530

    name change

--- pages/Stock_analysis.py
+++ pages/Stock_analysis.py
@@ -166,7 +166,7 @@ st.markdown(
     """
         <br><br><hr>
         <div style='text-align: center;'>
-            Developed by Unnat Malik, Seher Sarik, Teerth lalwani, Ojas Singwi
+            Developed by Unnat Malik, Seher Sharik, Teerth lalwani, Ojas Singwi
         </div>
     """, unsafe_allow_html=True
     )
